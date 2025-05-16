57a021659931c582eed248f37a4bca66a3f81989
DevEduardoReyes <jereyesb@unah.hn>
2025-05-16 17:48:03 -0600

    primer commit base del proyecto

---
+++ curso.js
@@ -0,0 +1,22 @@
+document.write(" <h1>Curso de JavaScript</h1>","<p>Curso de JavaScript</p>")
+
+document.write(" <h2>estoy probando la conexion javascript con html</h2>")
+
+let n1= ' eduardo ';
+var n2 = ' dimas ';
+const n3 = ' mari';
+
+document.writeln(n1,n2,n3);
+
+n2 = 'adalid';
+
+n1 = 'reyes'
+
+document.writeln(n1,n2,n3);
+
+let number1 = 10;
+let number2 = 20;
+number1 = 20;
+let number3 = number1 == number2;
+
+document.writeln(number3);
