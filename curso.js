49d8835cfa83ff90723b52a58b536b2ed3a00338
DevEduardoReyes <jereyesb@unah.hn>
2025-06-18 08:07:38 -0600

    funciones integradas y escalado

--- curso.js
+++ curso.js
@@ -2,3 +2,9 @@ document.querySelector('.menu-responsive').addEventListener('click', () => {
     
     document.querySelector('nav ul').classList.toggle('show');
 });
+
+document.querySelectorAll('nav ul li a').forEach((enlace) => {
+    enlace.addEventListener('click', () => {
+      document.querySelector('nav ul').classList.remove('show');
+    });
+  });
